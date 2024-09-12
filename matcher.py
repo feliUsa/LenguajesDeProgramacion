ac39bda9ef70c054dc78b03ac916a48dfcb34c0e
feliUSa <daniel.oviedo01@usa.edu.co>
2024-09-12 12:17:22 -0500

    add self

--- matcher.py
+++ matcher.py
@@ -10,7 +10,8 @@ class Matcher:
             'False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 
             'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 
             'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 
-            'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'
+            'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield',
+            'AssempionError', 'self'
         }
         self.token_patterns = [
             ('tk_and_sym', '&&'),
